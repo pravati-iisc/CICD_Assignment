2ac68dccfd600cac0e7953515b549360951d4b7e
pravati-iisc <164405450+pravati-iisc@users.noreply.github.com>
2024-04-08 00:59:29 +0530

    Update train.py
    one action fail

--- train.py
+++ train.py
@@ -2,14 +2,14 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
-
+ 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
-y = np.array([np.where(labels == x) for x in y]).flaten()
-
-model = LogisticRegression().fit(X, y)
-
+y = np.array([np.where(labels == x) for x in y]).flatten()
+ 
+model = LogisticRegression(C=100).fit(X, y)
+ 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
