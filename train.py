bcf5b29b1b0b3547935d35c5990fdab201603381
pravati-iisc <164405450+pravati-iisc@users.noreply.github.com>
2024-04-08 09:38:36 +0530

    Update train.py (#10)
    Both action has passed

--- train.py
+++ train.py
@@ -1,17 +1,21 @@
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import GradientBoostingClassifier
 import pickle
-import numpy as np
- 
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+
+# Read training data
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
-#added lebels 
-labels = np.array(['Anemia', 'Diabetes', 'Healthy', 'Heart Di', 'Thalasse', 'Thromboc'])
-y = np.array([np.where(labels == x) for x in y]).flatten()
- 
-model = GradientBoostingClassifier(max_depth = 3, n_estimators = 500, learning_rate = 0.05).fit(X,y)
- 
+
+# To convert categorical variables
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+# Train model
+gm = GaussianNB()
+gm.fit(X, y_encoded)
+
+# save model
 with open("model.pkl", 'wb') as f:
-    pickle.dump(model, f)
+    pickle.dump(gm, f)
