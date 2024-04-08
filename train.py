42a7be4ec3479075f004862234609923bf6c6912
pravati-iisc <164405450+pravati-iisc@users.noreply.github.com>
2024-04-08 09:34:19 +0530

    Update train.py
    Both action fails

--- train.py
+++ train.py
@@ -2,14 +2,18 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
- 
+
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
- 
-model = LogisticRegression(solver='adam').fit(X, y)
- 
-with open("model.pkl", 'wb') as f:
-    pickle.dump(model, f)
+
+model = LogisticRegression().fit(X, y)
+
+try:
+    with open("model.pkl", 'wb') as f:
+        pickle.dump(mod, f)
+except Exception as e:
+    print('\n\n## Creating Manual error for forcing it to fail. ##\n\n')
+    raise
